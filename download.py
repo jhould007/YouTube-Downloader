69793f0ec9456c8a5fc7d788740cc6417d66a23b
Josh Houlding <jdh102040@gmail.com>
2022-06-27 12:22:50 -0700

    Updated comments

--- download.py
+++ download.py
@@ -6,6 +6,8 @@ from pytube import Playlist
 # HOW TO USE
 # 1) Write a function call in one of the formats shown below, depending on if you want the playlist as video or audio only.
 # 2) Open a terminal window and type "python download.py" without the quotes.
+# 3) The videos will start downloading into the root directory. Once the process is done, you can move them wherever you like.
+# 4) Enjoy!
 
 # Downloads a playlist as mp4 audio
 def downloadPlaylistAudio(url):
